049cd7eccb713fd2faa66c4cf80c41d61fb853c2
Senash Thenuja <senathenu@gmail.com>
2024-09-08 23:15:35 +1200

    🔖 chore: add project files.

---
+++ MainForm.cs
@@ -0,0 +1,15 @@
+namespace PaperTracker
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
